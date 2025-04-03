e61327d6ee2d5385a4d1cc7fa57ac8de00897cce
awei0316 <2728318437@qq.com>
2025-04-03 21:36:28 +0800

    修改背景

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,18 +4,13 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="container">
-                <div className="footer-links">
-                    <div>
-                        <h3>关于我们</h3>
-                        <a href="/about">集团简介</a>
-                        <a href="/contact">联系我们</a>
-                    </div>
+                <div className="footer-links footer-content-adjustment">
                     <div>
                         <h3>客户服务</h3>
-                        <a href="#">客服热线：0351-7021781</a>
+                        <p>客服热线：0351-7021781</p>
                     </div>
                 </div>
-                <div className="copyright">
+                <div className="copyright footer-content-adjustment">
                     <p>版权所有©️ 河北工程大学</p>
                     <p>冀公网安备 3415535123525号 | 工信部备案号：冀ICP备312314115号-5</p>
                 </div>
@@ -24,4 +19,4 @@ const Footer = () => {
     );
 };
 
-export default Footer;    
+export default Footer;
