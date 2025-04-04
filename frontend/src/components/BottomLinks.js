423b4c55c4573dc61cd586f25840165793c8c8e0
awei0316 <2728318437@qq.com>
2025-04-04 21:00:45 +0800

    登录注册页面优化

--- frontend/src/components/BottomLinks.js
+++ frontend/src/components/BottomLinks.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const BottomLinks = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+            if (scrollTop + clientHeight >= scrollHeight - 50) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <div className="bottom-links-container">
+        <div className={`bottom-links-container ${isVisible ? 'show' : 'hide'}`}>
             <div className="bottom-left-link">
                 <Link to="/about">关于我们</Link>
             </div>
